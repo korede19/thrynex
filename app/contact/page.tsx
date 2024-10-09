7ac76d4b176fc0469c3d4215633390d4fbb96aa9
apple <mac@MACs-MacBook-Pro.local>
2024-10-09 10:57:29 +0100

    update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,6 @@
 import Contact from "@/components/contact";
 import Footer from "@/components/foooter";
 import Header from "@/components/header";
-import OtherHeros from "@/components/otherHeros";
 import React from "react";
 
 const ContactUS = () => {
